82f446f2207f752e24bbd57183bcb1334b67cd83
Carlos Herbert <hrbrtvieira@gmail.com>
2025-03-25 12:44:30 -0300

    continuando logica de tradução dos elementos renderizados

--- packages/web/src/components/projects/projects.tsx
+++ packages/web/src/components/projects/projects.tsx
@@ -7,26 +7,28 @@ import { useEffect, useState } from "react"
 export function Projects() {
   const [projects, setProjects] = useState<Apidata[]>([]);
   // const [text, setText] = useState('en');
-  const [teste, setTeste] = useState('en')
-  const [texts, setTexts] = useState<string[]>(["Olá, mundo!", "Bem-vindo ao site!", "Tradução automática"])
-
+  const [teste, setTeste] = useState('pt')
+  const [translated, setTranslated] = useState<string[]>([])
+  const texts = ["Olá, mundo!", "Bem-vindo ao site!", "Tradução automática"]
   useEffect(() => {
     const fecthdetails = async () => {
       const response = await getInfo('curriculum')
-      const translated = await translateText(texts, teste)
-      console.log(translated);
-      setTexts(translated ?? [])
       setProjects(response)
-      console.log(response)
+      const trans = response.map((project) => {
+        return {title: project.title, description: project.description}
+      })
+      console.log(trans)
+      
+      const translated = await translateText(texts, 'en')
+      // const tran = await translateText(trans, 'en')
+      // setTranslated([translated, tran])
       
 
       
 
     }
     fecthdetails()
-  }, [teste])
-
-  console.log(projects);
+  }, [])
   
   // async function testando(params: string) {
   //   const translated = await translateText(params, 'en')
@@ -37,8 +39,28 @@ export function Projects() {
   return (
     <>
             <h1>
-              {texts[1]}
+              { teste === 'en' ? translated[1] : texts[1]}
               </h1>
+            {projects.map((project) => (
+              <div key={project._id}>
+
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+
+                
+              </div>
+            ))}
+
+
+
+
+
+
+
+
+
+
+
             <button onClick={() => teste === 'en' ? setTeste('pt') : setTeste('en')}>
             {texts[2]}
             </button>
