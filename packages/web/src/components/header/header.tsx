8ef15dd7bac4e0ac27648ca6cabca4c695586c9e
CarlosHerbertDev <hrbrtvieira@gmail.com>
2025-05-15 17:04:14 -0300

    continuando estilos

--- packages/web/src/components/header/header.tsx
+++ packages/web/src/components/header/header.tsx
@@ -42,54 +42,44 @@ export const Header = (): ReactElement => {
     return (
 
             <header className="container justify-between">
-                <div className="flex justify-center items-center flex-row gap-[10px]">
+                <div className="flex justify-start items-center flex-row gap-[10px]">
+                        <div className="relative w-full inline-block">
+                            <button 
+                                className={`px-1 py-1 w-full cursor-pointer bg-white-400 text-yellow-800 border-1 border-indigo-800 ${isDropdownOpen ? 'rounded-t-sm' : 'rounded-sm'}`}
+                                onClick={() => setDropdownOpen(!isDropdownOpen)}>
+                                    <div className="flex justify-center items-center gap-[7px]">
+                                        {selectedLang && selectedLang.label}
+                                        {selectedLang && <img src={selectedLang.flag} alt={selectedLang.label} />}
+                                    </div>
+                                        {isDropdownOpen && (
+                                        <ul className="absolute right-0 left-0 top-full z-1 rounded-b-sm bg-gray-950 text-white p-0 max-w-[80px] overflow-auto">
+                                            {
+                                                languages.map((lang) => (
+                                                    <li 
+                                                        key={lang.name} 
+                                                        className = 'itemdrop'
+                                                        onClick={() => handleChange(lang.name)}>
+                                                            <div className="flex justify-center items-center gap-[7px]">
+                                                                {lang.label}
+                                                                <img src={lang.flag} alt={lang.label} />
+                                                            </div>
+                                                    </li>
+                                                ))
+                                            }
+                                        </ul>
+                                    )}
+                            </button>
+                        </div>
+                <div className="flex w-full ">
                     <p>
                         CarlosHerbertDev
                     </p>
-
+                </div>      
+                    </div>
                     {/* <button onClick={(() => lang === 'en' ? setLang('pt') : setLang('en'))}>
                         <img src={lang === 'en'? `${brasil}` : `${eua}`} alt="" />
-                    </button> */}
+                        </button> */}
 
-                    <div className="relative w-full inline-block">
-                        <button 
-                            className={`px-1 py-1 w-full cursor-pointer bg-green-400 text-yellow-800 border-1 border-indigo-800 ${isDropdownOpen ? 'rounded-t-sm' : 'rounded-sm'}`}
-                            onClick={() => setDropdownOpen(!isDropdownOpen)}>
-                                <div className="flex justify-center items-center gap-[7px]">
-                                    {selectedLang && selectedLang.label}
-                                    {selectedLang && <img src={selectedLang.flag} alt={selectedLang.label} />}
-                                </div>
-                                    {isDropdownOpen && (
-                                    <ul className="absolute right-0 left-0 top-full z-1 rounded-b-sm bg-gray-950 text-white p-0 max-w-[150px] overflow-auto">
-                                        {/* <li 
-                                            className = 'itemdrop'
-                                            onClick={() => handleChange('pt')}> 
-                                                pt 
-                                        </li>
-<li 
-                                            className = 'itemdrop'
-                                            onClick={() => handleChange('en')}> 
-                                                en 
-                                        </li>
-                                                                             */}
-                                        {
-                                            languages.map((lang) => (
-                                                <li 
-                                                    key={lang.name} 
-                                                    className = 'itemdrop'
-                                                    onClick={() => handleChange(lang.name)}>
-                                                        <div className="flex justify-center items-center gap-[7px]">
-                                                            {lang.label}
-                                                            <img src={lang.flag} alt={lang.label} />
-                                                        </div>
-                                                </li>
-                                            ))
-                                        }
-                                    </ul>
-                                )}
-                        </button>
-                    </div>
-                </div>
                 
                 
                 {isMobile ? (
