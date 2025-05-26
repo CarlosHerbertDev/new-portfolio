558013caba0d774b6d099ac37459fe36e1672a14
CarlosHerbertDev <hrbrtvieira@gmail.com>
2025-05-26 16:30:53 -0300

    modificando fundo animado

--- packages/web/src/components/plano-de-fundo.tsx
+++ packages/web/src/components/plano-de-fundo.tsx
@@ -1,15 +1,24 @@
-import { ReactElement } from "react"
-import "./plano.css"
+import { ReactElement } from "react";
+import "./plano.css";
+
+const NUM_SHOOTING_STARS = 8;
 
 export const Plano = (): ReactElement => {
   return (
-    <div className="container">
+    <div className="background-container">
       <div className="stars"></div>
-        <div className="shooting-star"></div>
-        <div className="shooting-star"></div>
-        <div className="shooting-star"></div>
-        <div className="shooting-star"></div>
-        <div className="shooting-star"></div>
+      {Array.from({ length: NUM_SHOOTING_STARS }).map((_, index) => (
+        <div
+          key={index}
+          className="shooting-star"
+          style={{
+            top: `${Math.random() * 100}vh`,
+            left: `-${Math.random() * 200}px`,
+            animationDelay: `${Math.random() * 10}s`,
+            animationDuration: `${3 + Math.random() * 3}s`,
+          }}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
