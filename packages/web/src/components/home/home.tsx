d81dcf2327ddb7bb16a680c9007106104527e7fd
CarlosHerbertDev <hrbrtvieira@gmail.com>
2025-06-16 16:48:47 -0300

    criando estrutura da home

--- packages/web/src/components/home/home.tsx
+++ packages/web/src/components/home/home.tsx
@@ -1,12 +1,25 @@
 // import { useContentApi } from "@hooks/useContentApi";
 // import { motion } from "motion/react";
 import { ReactElement } from "react";
+import image from "@assets/perfil.jpg";
 
 export const Home = (): ReactElement => {
 // const { dataAPI } = useContentApi('home')
 
 return(
-        <section className="container flex-col text-center justify-center">
+        <section className="container flex-col text-center justify-center items-center mt-20">
+
+            <div className="w-full flex items-center justify-between">
+                <div className="w-full max-w-1/2 flex flex-col gap-4 items-center justify-center">
+                    <h1 className="text-4xl text-center">Olá! Meu nome é Herbert</h1>
+                    <h2 className="text-center">Desenvolvedor FullSatck</h2>
+                </div>
+                <div className="w-full max-w-1/2 flex items-center justify-center">
+                    <img src={image} alt="" className="w-[350px] h-[320px] rounded-[50%]"/>
+                </div>
+            </div>
+
+
             {/* {dataAPI.map((data) => {
                 return (
                     <div key={data._id}>
